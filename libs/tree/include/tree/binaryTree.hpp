c70d9f2ab5b652a6634945c63d35e707137d79d6
nova <nova@192.168.2.192>
2018-08-23 23:30:08 +0800

    add

---
+++ libs/tree/include/tree/binaryTree.hpp
@@ -0,0 +1,30 @@
+//
+// Created by nova on 2018/8/23.
+//
+
+
+#pragma once
+
+
+
+namespace zero{
+
+    template <typename T>
+    struct binaryTree{
+        typedef T       type;
+
+        binaryTree(){}
+        binaryTree(const T &ele):element(ele){}
+        binaryTree(const T &theElement, const T *rightEle, const T *leftEle)
+        :element(theElement),rightChild(rightEle), leftChild(leftEle){
+
+        }
+
+
+        binaryTree<type>    *leftChild;
+        binaryTree<type>    *rightChild;
+        type    element;
+    };
+
+
+}
