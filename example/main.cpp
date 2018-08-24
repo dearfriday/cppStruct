625269e0784523d8214a290bde4e0070a63fd13b
dearfriday <dream2053@vip.qq.com>
2018-08-24 18:02:09 +0800

    update

--- example/main.cpp
+++ example/main.cpp
@@ -3,11 +3,22 @@
 //
 
 #include <map>
+#include <vector>
+#include <iostream>
+
+
+
+
 
 int main(int argc, char **argv){
-
-
-    std::map<int, int> mm;
-
+    std::map<int32_t, int32_t> vv;
+    vv[1] = 1;
+    vv[2] = 2;
+    vv.operator[](55) = 1;
+    vv.operator[](54) = 5;
+    for(auto itr = vv.begin(); itr != vv.end(); itr++){
+        std::cout <<  "value " << itr->second << std::endl;
+        std::cout << " key : " << itr->first << std::endl;
+    }
     return 0;
 }
