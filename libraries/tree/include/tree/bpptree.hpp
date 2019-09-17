bda5b073c36aa994f2c9c497e9c61fda2da1a6f7
magicbaer <magicbaer@baerchain.net>
2019-09-17 22:37:39 +0800

    sss

--- libraries/tree/include/tree/bpptree.hpp
+++ libraries/tree/include/tree/bpptree.hpp
@@ -13,7 +13,11 @@ namespace zero{
 
     template <typename KEY, size_t M = 1024, typename Compare = std::less<KEY>>
     struct bpptree{
-        bpptree():m_parent(nullptr){
+
+
+        static_assert(M >= 3, "bpptree M must > 3");
+
+        bpptree(bpptree *parent = nullptr):m_parent(parent){
 
         }
 
@@ -21,18 +25,53 @@ namespace zero{
         typedef KEY                    key_type;
 
         void insert(const key_type &key){
-            size_t insert_index = 0;
+            bpptree * insert_node = find_index_node(key);
+            insert_node->insert_value_to_node(key);
+        }
 
-            auto &insert_node = find_index_node(key);
-            insert_node.insert_value_to_node(key);
 
+        bool empty(){
+            return m_values.size() == 0;
+        }
+
+        ~bpptree(){
+            m_parent = nullptr;
+        }
+
+    private:
+
+        /// insert value and leaf to parent
+        /// \param key      insert  value
+        /// \param first    left    value
+        /// \param second   right   value
+        /// \return parent
+        bpptree *insert_value_leaf_to_node(key_type &&key,  bpptree<key_type, M, Compare> &&first,  bpptree<key_type, M, Compare> &&second)
+        {
+            assert(this->m_parent);
+            bpptree *insert_node = this->m_parent;
+            auto  insert_index = insert_node->find_insert_index(key);
+            if(!insert_index.second){
+                insert_node->m_values.push_back(key);
+                for(size_t i = insert_node->m_values.size() - 1; i > insert_index.first; i--){
+                    std::swap(insert_node->m_values[i], insert_node->m_values[i - 1]);
+                }
+
+                assert(insert_node->m_leaf.size() >= insert_index.first);
+                std::swap(insert_node->m_leaf[insert_index.first], first);
+                insert_node->m_leaf.push_back(second);
+                for(size_t i = insert_node->m_leaf.size() - 1; i > insert_index.first + 1; i--){
+                    std::swap(insert_node->m_leaf[i], insert_node->m_leaf[i - 1]);
+                }
+
+            }
+            return insert_node;
         }
 
 
 
-
-    private:
-
+        /// @brief  insert value to node , if node value.size == M + 1, try compose all values to parent.
+        /// \param key      value
+        /// \return
         void insert_value_to_node(const key_type &key){
             auto  insert_index = find_insert_index(key);
             if(!insert_index.second){
@@ -43,57 +82,123 @@ namespace zero{
             }
             else{
                 m_values[insert_index.first] = key;
+                return ;
             }
 
-        }
+            bpptree *insert_parent = this;
+            while (insert_parent != nullptr && !insert_parent->empty()){
+                if(insert_parent->m_values.size() == M + 1){
+                    size_t mid_length = insert_parent->m_values.size() / 2 + 1;
 
-        std::pair<size_t, bool> find_insert_index(const key_type &key){
-            size_t mid = m_values.size() > 0 ? m_values.size() / 2 : 0;
-            size_t add = m_values.size();
-            size_t flag = 0;
-            while (add > 0 && m_values.size()){
-                if (m_compare(key, m_values[mid])) {
-                    add = add > 0 ? add / 2 : 0;
-                    mid = mid - add;
-                } else if (m_compare(m_values[mid], key)) {
-                    add = add > 1 ? add / 2 : 1;
-                    mid = mid + add;
-                } else {
-//                    std::cout << "eq ? " << mid << std::endl;
-//                    break;
-                    return {mid, true};
-                }
-            }
-            return {mid, false};
-        }
+                    bpptree *parent = insert_parent->m_parent != nullptr ? insert_parent->m_parent : insert_parent;
+                    bpptree<key_type, M, Compare>   front_tree(parent);
+                    bpptree<key_type, M, Compare>   back_tree(parent);
+                    key_type                mid_value = insert_parent->m_values[mid_length - 1];
 
-        bpptree &find_index_node(const key_type &key){
-            bpptree *insert_node = this;
-            bool find = false;
-            while (!find){
-                size_t  size = insert_node->m_values.size();
-                if(size == 0){
-                    return  *insert_node;
-                }
-                if(!insert_node->m_compare(insert_node->m_values.front(), key) ){
-                    if(insert_node->m_leaf.size() > 0){
-                        insert_node = &insert_node->m_leaf.front();
+                    auto begin = insert_parent->m_values.begin();
+                    auto mid = begin + mid_length - 1;
+
+                    front_tree.m_values.resize(mid_length - 1);
+                    std::copy(begin, mid, front_tree.m_values.begin());
+                    back_tree.m_values.resize(insert_parent->m_values.size() - mid_length);
+                    std::copy(mid + 1, insert_parent->m_values.end(), back_tree.m_values.begin());
+
+                    if(insert_parent->m_leaf.size()){
+                        if(insert_parent->m_leaf.size() <= mid_length){
+                            front_tree.m_leaf.resize(insert_parent->m_leaf.size());
+                            std::copy(insert_parent->m_leaf.begin(), insert_parent->m_leaf.end(), front_tree.m_leaf.begin());
+                        }
+                        else if(insert_parent->m_leaf.size() > mid_length){
+                            front_tree.m_leaf.resize(mid_length);
+                            back_tree.m_leaf.resize(insert_parent->m_leaf.size() - mid_length);
+                            std::copy(insert_parent->m_leaf.begin(), insert_parent->m_leaf.begin() + mid_length, front_tree.m_leaf.begin());
+                            std::copy(insert_parent->m_leaf.begin() + mid_length, insert_parent->m_leaf.end(), back_tree.m_leaf.begin());
+                        }
+                    }
+
+
+                    if(insert_parent->m_parent != nullptr){
+                        insert_parent = insert_parent->insert_value_leaf_to_node(std::move(mid_value), std::move(front_tree), std::move(back_tree));
                     }
                     else{
-                        find = true;
+                        insert_parent->m_values.clear();
+                        insert_parent->m_leaf.clear();
+
+                        insert_parent->m_values.push_back(mid_value);
+                        insert_parent->m_leaf.push_back(front_tree);
+                        insert_parent->m_leaf.push_back(back_tree);
+
+
+                        for(auto &itr : insert_parent->m_leaf){
+                            for(auto &it : itr.m_leaf){
+                                it.m_parent = &itr;
+                            }
+                        }
+
+                        insert_parent = insert_parent->m_parent;
                     }
+
+
                 }
-                else if(!insert_node->m_compare(key, insert_node->m_values.back())){
-                    if(insert_node->m_leaf.size() > 0){
-                        insert_node = &insert_node->m_leaf.back();
-                    } else{
-                        find = true;
-                    }
-                } else{
-                    find = true;
+                else{
+                    return;
                 }
+
             }
-            return *insert_node;
+
+
+
+        }
+
+        /// @brief  find index by key.
+        /// \param key
+        /// \return  @param first insert value index , @param second this value is existed.
+        std::pair<size_t, bool> find_insert_index(const key_type &key){
+            size_t front = 0;
+            size_t end = m_values.size() - 1;
+            size_t mid = (front + end) / 2;
+            bool same = false;
+
+            if(m_values.size() == 0){
+                return {0, false};
+            }
+
+            while (front < end ) {
+                if (m_compare(key, m_values[mid])) {
+                    end = mid > 0 ? mid - 1 : mid;
+                } else if (m_compare(m_values[mid], key)) {
+                    front = mid + 1;
+                } else {
+                    same = true;
+                    break;
+                }
+                mid = (front + end) / 2;
+            }
+
+            if(m_compare(key, m_values[mid])){
+
+            }else if (m_compare(m_values[mid], key)) {
+                mid++;
+            } else {
+                same = true;
+            }
+            return {mid, same};
+        }
+
+        /// @brief      find node of inserted.
+        /// \param key  value.
+        /// \return     node point
+        bpptree *find_index_node(const key_type &key){
+            bpptree *insert_node = this;
+            while (insert_node->m_leaf.size()){
+                auto itr = insert_node->find_insert_index(key);
+                if(itr.second){     //same
+                    return insert_node;
+                }
+                assert(insert_node->m_leaf.size() >= itr.first);
+                insert_node = &insert_node->m_leaf[itr.first];
+            }
+            return insert_node;
         }
 
 
@@ -101,7 +206,7 @@ namespace zero{
         Compare                                             m_compare;
         std::vector<key_type>                               m_values;
         std::vector<bpptree<key_type, M, Compare>>          m_leaf;
-        std::shared_ptr<bpptree>                            m_parent;
+        bpptree                                             *m_parent;
     };
 
 
--- libraries/tree/test/main.cpp
+++ libraries/tree/test/main.cpp
@@ -15,6 +15,14 @@ struct book {
     size_t id = 0;
     std::string name;
 
+
+//    book& operator = (const book &b){
+//        id = b.id;
+//        name = b.name;
+//        return *this;
+//    }
+
+
     bool operator<(const book &b) const {
         return id < b.id;
     }
@@ -30,50 +38,64 @@ int main() {
 //    std::vector<size_t> test;
 //    test.push_back(1);
 //    test.push_back(3);
-//    test.push_back(5);
+//    test.push_back(5);  //2
 //    test.push_back(7);
 //    test.push_back(9);
-////    test.push_back(11);
-////    test.push_back(13);
 //
-//    size_t key = 5;
 //
-//    size_t mid = test.size() > 0 ? test.size() / 2 : 0;
-//    size_t add = mid;
+//
+//    size_t key = 9;
+//
+//    size_t front = 0;
+//    size_t end = test.size() - 1;
+//    size_t mid = (front + end) / 2;
 //
 //    auto func = [](const size_t &t1, const size_t &t2) {
 //        return t1 < t2;
 //    };
 //
-//    size_t flag = 0;
-//
-//    while (add > 0 && test.size()) {
+//    //&& test.size() > mid && mid > 0
+//    while (front < end ) {
 //        if (func(key, test[mid])) {
-//            add = add > 0 ? add / 2 : 0;
-//            mid = mid - add;
+//            end = mid > 0 ? mid - 1 : mid;
 //        } else if (func(test[mid], key)) {
-//            add = add > 1 ? add / 2 : 1;
-//            mid = mid + add;
+//            front = mid + 1;
 //        } else {
 //            std::cout << "eq ? " << mid << std::endl;
 //            break;
 //        }
-//
+//        mid = (front + end) / 2;
 //    }
+//
+//    if (func(test[mid], key)) {
+//        mid++;
+//    } else {
+//        std::cout << "eq ? " << mid << std::endl;
+//    }
+//
+//
+//
+//
+//
 //    std::cout << "index " << mid << std::endl;
 
+    try {
+        std::cout << "xxxxxxxxx\n";
+        bpptree<int, 4> books;
+        std::vector<int> data = {39 , 22, 97, 41, 53, 13, 21, 40, 30, 27, 33, 36, 35, 34, 24, 29, 26, 17, 28, 29, 31};
+        for(size_t i = 0; i < data.size(); i++){
+            books.insert(data[i]);
+        }
+        books.insert(32);
+        std::cout << "\n";
+    }catch (const std::exception &e){
+        std::cout << e.what() << std::endl;
+    }catch (...){
+        std::cout << "unkown exception ....";
+    }
 
-    std::cout << "xxxxxxxxx\n";
-    bpptree<book, 12> books;
-
-    books.insert({1, "1"});
-    books.insert({4, "4"});
-    books.insert({2, "2"});
-    books.insert({5, "6"});
-    books.insert({3, "3"});
 
 
-    std::cout << "\n";
 
 
     return 0;
